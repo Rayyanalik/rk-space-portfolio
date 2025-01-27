6747b893595f8fa87233b9d5d2c100e04bd4a0cd
Ali Rayyan Mohammed <67268145+Rayyanalik@users.noreply.github.com>
2025-01-27 00:04:23 -0600

    Update navbar.tsx

--- components/main/navbar.tsx
+++ components/main/navbar.tsx
@@ -18,7 +18,7 @@ export const Navbar = () => {
           className="flex items-center"
         >
           <Image
-            src="/wand.png"
+            src=""
             alt=""
             width={70}
             height={70}
