977d90310e24423fb6ec6220672212e8b77a345c
Ali Rayyan Mohammed <67268145+Rayyanalik@users.noreply.github.com>
2025-01-27 01:06:37 -0600

    Update hero-content.tsx

--- components/sub/hero-content.tsx
+++ components/sub/hero-content.tsx
@@ -52,7 +52,7 @@ export const HeroContent = () => {
         <motion.a
           variants={slideInFromLeft(1)}
           className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
-          href="/pinterest_resume.pdf"
+          href="pinterest_resume.pdf"
           target="_blank"
         >
           Resume
